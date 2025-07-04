c0d4e16b1d5b0d85cfc433f4363496b5cc6de191
Allan <153043227+All4mn@users.noreply.github.com>
2025-07-04 20:50:31 -0300

    implementação filtro

--- src/components/AddGasto.jsx
+++ src/components/AddGasto.jsx
@@ -36,8 +36,8 @@ const AddGasto = ({adicionarGasto}) => {
       <input type="number" value={valor} placeholder='Valor(obrigatório)'onChange={(e)=>(setValor(e.target.value))}/>
       <select value={tipo} onChange={(e)=>(setTipo(e.target.value))} >
           <option value="">Tipo</option>
-          <option value="fixo">Fixo</option>
-          <option value="variavel">Variavel</option>
+          <option value="Fixo">Fixo</option>
+          <option value="Variavel">Variavel</option>
       </select>
       <button className='botao'onClick={hoverAdd}>Adicionar</button>
     </div>
--- src/components/Filtro.jsx
+++ src/components/Filtro.jsx
@@ -1,9 +1,9 @@
 
-const Filtro = () => {
+const Filtro = ({filtrar, setFiltrar}) => {
   return (
     <div>
         <h2>Filtrar:</h2>
-        <select>
+        <select value={filtrar} onChange={(e) => setFiltrar(e.target.value)}>
             <option value="all">Todos</option>
             <option value="fix">Fixo</option>
             <option value="var">Variavel</option>
